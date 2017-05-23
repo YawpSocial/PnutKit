1e0e2322870b4f0ab7a38c4c620acaf754b03a2f
Paul Schifferer <paul@schifferers.net>
2017-05-23 10:08:11 -0700

    Remove UIKit from header.

--- PnutKit/PnutKit.h
+++ PnutKit/PnutKit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Pilgrimage Software. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for PnutKit.
 FOUNDATION_EXPORT double PnutKitVersionNumber;
